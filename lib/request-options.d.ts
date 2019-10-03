1fa68b589479be6f504e1d0f36db215be082ea2c
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-10-03 12:13:00 -0500

    feat: add lib files for testing

---
+++ lib/request-options.d.ts
@@ -0,0 +1,13 @@
+import Headers from './headers';
+import Timeout from './timeout';
+export default interface RequestOptions {
+    body?: any;
+    cache?: boolean;
+    credentials?: boolean;
+    headers?: Headers;
+    method?: string;
+    params?: {
+        [key: string]: any;
+    };
+    timeout?: Timeout | Promise<any>;
+}
